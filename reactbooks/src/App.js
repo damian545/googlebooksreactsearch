a6f29dcd042052a48f34e0ebf93ac37ead53930d
Damian Tapp <dam180@hotmail.com>
2020-07-15 20:52:07 +1000

    added all files and code

---
+++ reactbooks/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Header from "./header";
+import "./App.css";
+import Books from "./books";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Books />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ reactbooks/src/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <i className="fa fa-book fa-2x"></i>
+      <h1>React Book Search</h1>
+    </header>
+  );
+};
+
+export default Header;
