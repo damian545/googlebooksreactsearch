21781c1b106c603e8ae96e60daa0c9c16ff14c4c
Damian Tapp <dam180@hotmail.com>
2020-07-19 16:35:04 +1000

    added a change

--- reactbooks/src/header.js
+++ reactbooks/src/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <i className="fa fa-book fa-2x"></i>
-      <h1>React Book Search</h1>
+      <h1>REACT BOOK SEARCH</h1>
     </header>
   );
 };
